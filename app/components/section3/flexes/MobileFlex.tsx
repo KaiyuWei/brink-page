64edecd8b840a58157c7427c960bd30df98d7e29
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-10-03 22:56:05 +0200

    added mobile version in secion 3

---
+++ app/components/section3/flexes/MobileFlex.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "../section3.module.css";
+
+export default function MobileFlex() {
+  return (
+    <div className="px-14 flex-container flex-row content-between">
+      <div className={`${styles.flexTextTitle}`}>
+        <p className="title-2">
+          Proin enim enim, tincidunt consequat sollicitudin.
+        </p>
+      </div>
+      <div className={`${styles.flexText}`}>
+        <p className="plain-text">
+          Aliquam vel orci pulvinar, porta velit in, volutpat neque. Nam vel leo
+          nec nibh tempus congue. Morbi rhoncus bibendum enim sit amet pulvinar.
+          Quisque lobortis commodo nunc, a rhoncus diam aliquam ac. Pellentesque
+          non augue ullamcorper arcu facilisis bibendum. Nulla quis tincidunt
+          turpis. Sed quis erat arcu. Phasellus gravida felis quis ex mattis,
+          sed suscipit tellus tincidunt. In imperdiet dapibus vehicula. In
+          auctor nec nibh vestibulum ornare. Morbi porta, enim hendrerit
+          consectetur ultrices, libero dui ultricies libero, vel rutrum sem
+          magna quis augue. Nulla quis magna at lorem sodales gravida.
+        </p>
+      </div>
+      <div className={`placeholder-box ${styles.flexItem}`}></div>
+      <div className={`${styles.flexText}`}>
+        <p className="plain-text">
+          Phasellus gravida felis quis ex mattis, sed suscipit tellus tincidunt.
+          In imperdiet dapibus vehicula. In auctor nec nibh vestibulum ornare.
+          Morbi porta, enim hendrerit consectetur ultrices, libero dui ultricies
+          libero, vel rutrum sem magna quis augue. Nulla quis magna at lorem
+          sodales gravida.
+        </p>
+      </div>
+      <div
+        className={`placeholder-box ${styles.flexItem} ${styles.darker}`}
+      ></div>
+    </div>
+  );
+}
--- app/components/section3/section3.tsx
+++ app/components/section3/section3.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import styles from "./section3.module.css";
 import { useMediaQuery } from "react-responsive";
 import DesktopFlex from "./flexes/desktopFlex";
+import MobileFlex from "./flexes/mobileFlex";
 
 export default function Section3() {
   // media query to determine the rendering result
   const isDesktopOrLaptop = useMediaQuery({
-    query: "(min-width: 1224px)",
+    query: "(min-width: 1300px)",
   });
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 
   return (
     <div>
       <section className="dark-section">
         <div style={{ width: "100%", height: "8rem" }}>&nbsp;</div>
-        {/* the section that desplays in desktop media size */}
+        {/* desplay for desktops*/}
         {isDesktopOrLaptop && <DesktopFlex />}
+        {/* display for mobile devices*/}
+        {!isDesktopOrLaptop && <MobileFlex />}
         <div className={styles.whiteLine}></div>
       </section>
     </div>
