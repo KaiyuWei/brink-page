84c05b528b72b6b815938e0f34713af268de966c
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-10-03 23:14:56 +0200

    overlap boxes completed

--- app/components/section3/section3.tsx
+++ app/components/section3/section3.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./section3.module.css";
 import { useMediaQuery } from "react-responsive";
-import DesktopFlex from "./flexes/desktopFlex";
-import MobileFlex from "./flexes/mobileFlex";
+import DesktopFlex from "./flexes/DesktopFlex";
+import MobileFlex from "./flexes/MobileFlex";
 
 export default function Section3() {
   // media query to determine the rendering result
@@ -15,9 +15,10 @@ export default function Section3() {
       <section className="dark-section">
         <div style={{ width: "100%", height: "8rem" }}>&nbsp;</div>
         {/* desplay for desktops*/}
-        {isDesktopOrLaptop && <DesktopFlex />}
+        {/* {isDesktopOrLaptop && <DesktopFlex />} */}
         {/* display for mobile devices*/}
-        {!isDesktopOrLaptop && <MobileFlex />}
+        {/* {!isDesktopOrLaptop && <MobileFlex />} */}
+        <DesktopFlex />
         <div className={styles.whiteLine}></div>
       </section>
     </div>
