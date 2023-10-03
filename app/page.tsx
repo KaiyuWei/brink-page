4537682f984c167822e8cb887b532d69c1f65df1
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-10-03 11:10:21 +0200

    section 2 completed

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,16 @@ export default function Home() {
           <p className={styles.firstLevelTitle}>sit amet lacus</p>
         </div>
         <div className="flex-container">
-          <div className={`${styles.placeHolderBox} flex-container`}>
+          <div
+            className={`${styles.placeHolderBox} flex`}
+            style={{ width: "57rem", height: "65rem", margin: "3rem 0rem" }}
+          >
             &nbsp;
           </div>
-          <div className={`${styles.placeHolderBox} flex-container`}>
+          <div
+            className={`${styles.placeHolderBox} flex`}
+            style={{ width: "57rem", height: "65rem", margin: "3rem 0rem" }}
+          >
             &nbsp;
           </div>
         </div>
@@ -31,7 +37,7 @@ export default function Home() {
             <p className={`${styles.plainText} text-center`}>
               Suspendisse tincidunt vulputate ex, a laoreet neque luctus vitae.
               Quisque maximus ante sapien, at pharetra sem sagittis eu. Maecenas
-              nibh leo, interdum ac malesuada quis, tempus vel lacus.{" "}
+              nibh leo, interdum ac malesuada quis, tempus vel lacus.
               <div>
                 <br />
               </div>
@@ -41,9 +47,59 @@ export default function Home() {
             </p>
           </div>
         </div>
+        <div className="flex-container flex-row justify-center">
+          <div className="flex-col">
+            <div
+              className={`${styles.placeHolderBox} flex`}
+              style={{ width: "35rem", height: "23rem", margin: "3rem 0rem" }}
+            ></div>
+            <div
+              className={`${styles.placeHolderBox} flex`}
+              style={{ width: "35rem", height: "23rem", margin: "3rem 0rem" }}
+            ></div>
+          </div>
+          <div
+            className={`${styles.placeHolderBox} flex`}
+            style={{ width: "35rem", height: "49rem", margin: "3rem 0rem" }}
+          ></div>
+          <div className="flex-col">
+            <div
+              className={`${styles.placeHolderBox} flex`}
+              style={{ width: "35rem", height: "23rem", margin: "3rem 0rem" }}
+            ></div>
+            <div
+              className={`${styles.placeHolderBox} flex`}
+              style={{ width: "35rem", height: "23rem", margin: "3rem 0rem" }}
+            ></div>
+          </div>
+        </div>
       </section>
-      <section>Section 3</section>
-      <section>Section 4</section>
+      <section className="dark-section">
+        <div style={{ width: "100%", height: "8rem" }}>&nbsp;</div>
+        <div className="flex-container">
+          <div className="flex flex-row">
+            <h6 className={`${styles.secondLevelTitle} basis-1/2 flex-wrap`}>
+              Proin enim enim, tincidunt consequat sollicitudin.
+            </h6>
+            <p className={`${styles.plainText} basis-1/2 flex-wrap`}>
+              Aliquam vel orci pulvinar, porta velit in, volutpat neque. Nam vel
+              leo nec nibh tempus congue. Morbi rhoncus bibendum enim sit amet
+              pulvinar. Quisque lobortis commodo nunc, a rhoncus diam aliquam
+              ac. Pellentesque non augue ullamcorper arcu facilisis bibendum.
+              Nulla quis tincidunt turpis. Sed quis erat arcu.
+              <div>
+                <br />
+              </div>
+              Phasellus gravida felis quis ex mattis, sed suscipit tellus
+              tincidunt. In imperdiet dapibus vehicula. In auctor nec nibh
+              vestibulum ornare. Morbi porta, enim hendrerit consectetur
+              ultrices, libero dui ultricies libero, vel rutrum sem magna quis
+              augue. Nulla quis magna at lorem sodales gravida.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="dark-section">Section 4</section>
     </>
   );
 }
