0e031b7fa7130f41e76521ce5d752029ccf6229f
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-10-03 16:38:50 +0200

    section 1 completed

--- app/components/section1/section1.tsx
+++ app/components/section1/section1.tsx
@@ -1,25 +1,20 @@
-import "./section1.css";
+import React from "react";
+import styles from "./section1.module.css";
 
 export default function Section1() {
   return (
     <div>
-      <div style={{ width: "100%", height: "10em" }}></div>
+      <div id={styles.sectionOneHeader}>&nbsp;</div>
       <div>
         <p className="plain-text">Luctus vitae.</p>
         <h1 className="title-1">Utrum ut placerat nec, varius</h1>
         <p className="title-1">sit amet lacus</p>
       </div>
-      <div className="flex-container">
-        <div
-          className="placeholder-box flex"
-          style={{ width: "57rem", height: "65rem", margin: "3rem 0rem" }}
-        >
+      <div className="flex-container justify-between">
+        <div className={`placeholder-box ${styles.flexItem} ${styles.left}`}>
           &nbsp;
         </div>
-        <div
-          className="placeholder-box flex"
-          style={{ width: "57rem", height: "65rem", margin: "3rem 0rem" }}
-        >
+        <div className={`placeholder-box ${styles.flexItem} ${styles.right}`}>
           &nbsp;
         </div>
       </div>
