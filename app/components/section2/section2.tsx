d20925df12d75ba974d1617b8552c67a65bcd79b
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-10-04 01:20:17 +0200

    desktop component added to section 2

--- app/components/section2/section2.tsx
+++ app/components/section2/section2.tsx
@@ -2,6 +2,7 @@ import styles from "./section2.module.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import { useMediaQuery } from "react-responsive";
+import DesktopFlex from "./flexes/DesktopFlex";
 
 export default function Section2() {
   // the view is for desktop of mobile devices
@@ -39,27 +40,7 @@ export default function Section2() {
           </div>
         </div>
         <div style={{ width: "100%", height: "8em" }}></div>
-        <div className="flex-container flex-row justify-center">
-          <div className={styles.flexVertical}>
-            <div className={`${styles.sideFlexItem} placeholder-box`}>
-              <img src="https://picsum.photos/id/35/5000/3333" />
-            </div>
-            <div className={`${styles.sideFlexItem} placeholder-box`}>
-              <img src="https://picsum.photos/id/1/5000/3333" />
-            </div>
-          </div>
-          <div className={`${styles.middleFlexItem} placeholder-box`}>
-            <img src="https://fastly.picsum.photos/id/227/560/780.jpg?hmac=Itg52EbSgUK99Q6Pw6tWslBDDdjrRTmbgvdXU6mpSrw" />
-          </div>
-          <div className={styles.flexVertical}>
-            <div className={`${styles.sideFlexItem} placeholder-box`}>
-              <img src="https://picsum.photos/id/23/5000/3333" />
-            </div>
-            <div className={`${styles.sideFlexItem} placeholder-box`}>
-              <img src="https://picsum.photos/id/12/5000/3333" />
-            </div>
-          </div>
-        </div>
+        <DesktopFlex />
         <div style={{ width: "100%", height: "11em" }}></div>
       </section>
     </div>
