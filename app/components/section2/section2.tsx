c41c6e5787a780959bd1e8a35376dd464d39ecae
Kaiyu Wei <kaiyuwei@Kaiyus-MacBook-Air.local>
2023-10-04 23:00:16 +0200

    section two context solved

--- app/components/section2/section2.tsx
+++ app/components/section2/section2.tsx
@@ -19,39 +19,33 @@ export default function Section2() {
 
   return (
     <div>
-      <section className="my-2">
-        <div id={`${styles.sectionTwoHeader}`}></div>
-        <div className="flex-container justify-center">
-          <div
-            className="flex-col items-center flex-container"
-            id={`${styles.sectionTwoTextBox}`}
-          >
-            <div className={styles.title}>
-              <p className="titleTwo text-center">
-                Suspendisse tincidunt vulputate ex, a laoreet neque luctus
-                vitae.
-              </p>
-            </div>
-            <div className="my-2 py-2">
-              <p className="plainText text-center">
-                Suspendisse tincidunt vulputate ex, a laoreet neque luctus
-                vitae. Quisque maximus ante sapien, at pharetra sem sagittis eu.
-                Maecenas nibh leo, interdum ac malesuada quis, tempus vel lacus.
-                <br />
-                <br />
-                Aliquam quis velit et nunc scelerisque auctor quis id lacus.
-                Proin sem turpis, rutrum ut placerat nec, varius sit amet lacus.
-                Praesent sed viverra lorem.
-              </p>
-            </div>
+      <div id={`${styles.sectionTwoHeader}`} />
+      <div>
+        <div className={styles.textContainer}>
+          <div className={styles.title}>
+            <p className="titleTwo text-center">
+              Suspendisse tincidunt vulputate ex, a laoreet neque luctus vitae.
+            </p>
+          </div>
+          <div className="my-2 py-2">
+            <p className="plainText text-center">
+              Suspendisse tincidunt vulputate ex, a laoreet neque luctus vitae.
+              Quisque maximus ante sapien, at pharetra sem sagittis eu. Maecenas
+              nibh leo, interdum ac malesuada quis, tempus vel lacus.
+              <br />
+              <br />
+              Aliquam quis velit et nunc scelerisque auctor quis id lacus. Proin
+              sem turpis, rutrum ut placerat nec, varius sit amet lacus.
+              Praesent sed viverra lorem.
+            </p>
           </div>
         </div>
-        <div id={`${styles.sectionTwoWaist}`}></div>
-        {isDesktop && <DesktopFlex />}
-        {isTablet && <TabletFlex />}
-        {isMobile && <MobileFlex />}
-        <div id={`${styles.sectionTwoFooter}`}></div>
-      </section>
+      </div>
+      <div id={`${styles.sectionTwoWaist}`} />
+      {isDesktop && <DesktopFlex />}
+      {isTablet && <TabletFlex />}
+      {isMobile && <MobileFlex />}
+      <div id={`${styles.sectionTwoFooter}`} />
     </div>
   );
 }
